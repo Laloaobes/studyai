8dc4b766827753a056efd46060caf33404af7ef8
Eduardo Lezama <elezama625@gmail.com>
2026-06-02 22:19:35 -0600

    feat: inicialización del proyecto studyai

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-indigo-600">StudyAI - Configuración Base Exitosa</h1>
+    </div>
+  )
+}
